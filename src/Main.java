c9d1de2136c45fbcfeb10bb5f4e352c5ba209b44
Noah Penza <penzanoah@gmail.com>
2021-08-02 10:09:56 +1000

    Completed Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
+        Main window = new Main();
         System.out.println("~~~~~");
         System.out.println("Red Vs Blue");
         System.out.println("~~~~~");
+
+    }
+    public class Canvas extends JPanel {
+        public Canvas(){
+            setPreferredSize(new Dimension(720,720));
+        }
+            @Override
+            public void paint (Graphics g){
+                for (int i = 10; i < 710; i += 35){
+                    for (int j = 10; j < 710; j+= 35){
+                        g.setColor(Color.WHITE);
+                        g.fillRect(i, j, 35, 35);
+                        g.setColor(Color.BLACK);
+                        g.drawRect(i, j, 35, 35);
+                    }
+               
+                }
+            }
+        }
+    
+    public Main(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
     }
 }
